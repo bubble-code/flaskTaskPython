c3af17d45e9b52592176e5d86b07a32541524a9b
Alejandro Obregón Rodríguez <a.obregon@favram.local>
2023-10-01 12:55:09 -0700

    Obtenida la estructura de la db

--- Models/AIQuery.py
+++ Models/AIQuery.py
@@ -1,12 +1,15 @@
 import openai
+import json
+
 
 class AIQuery:
     def __init__(self):
-        #openai.api_key = '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'
-        openai.api_key = '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'
-        self.context = [ {
-            'role':'system', 
-            'content':"""you are a bot to assist in create SQL commands, all your answers should start with \
+        self.ruta_file = r"C:\Users\a.obregon\source\repos\flaskTaskPython\Models\estructura_tablas.json"
+        # openai.api_key = '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'
+        # openai.api_key = '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'
+        self.context = [{
+            'role': 'system',
+            'content': """you are a bot to assist in create SQL commands, all your answers should start with \
                 this is your SQL, and after that an SQL that can do what the user request.\
                     Your Database is composed by a SQL database with some tables. \
                         Try to Maintain the SQL order simple. Put the SQL command in white letters with a black background, and just after \
@@ -14,29 +17,48 @@ class AIQuery:
                                 just answer something nice and simple and ask him for something that \
                                     can be solved with SQL.
                                     """
-                                    } ]
-         
-    def Hacer_pregunta(self,prompt,max_tokens=100,temperature=0.5,top_p=1.0, frequency_penalty=0.0,presence_penalty=0.0, stop=["\n"]):
-        self.context.append( {'role':'system', 'content':"""first table: { "tableName": "employees", "fields": [ { "nombre": "ID_usr", "tipo": "int" }, { "nombre": "name", "tipo": "string" }]}"""})
-        self.context.append( {'role':'system', 'content':"""second table:{"tableName": "salary","fields":[{"nombre": "ID_usr","type": "int"},{"name": "year","type": "date"},{"name": "salary","type": "float"}]}"""})
-        self.context.append( {'role':'system', 'content':"""third table:{"tablename": "studies","fields": [{"name": "ID","type": "int"},{"name": "ID_usr","type": "int"},{"name": "educational level","type": "int"},{"name": "Institution","type": "string"},{"name": "Years","type": "date"}{"name": "Speciality","type": "string"}]}"""})
-        self.context.append({'role':'user', 'content':f"{prompt}."})
-        self.context.append({'role':'system', 'content':r"Remember your instructions as SQL Assistant."})
+        }]
+
+    def Get_context_data(self):
+        try:
+            with open(self.ruta_file, "r") as file:
+                return json.load(file)
+                # return self.estructure_db
+        except Exception as e:
+            print("Error al cargar", e)
+            exit(1)
+
+    def add_contents(self):
+        json_data = self.Get_context_data()
+        contents = json_data["content"]
+        for content in contents:
+            table_name = content["tableName"]
+            self.context.append(
+                {'role': 'system', 'content': f"{table_name}:{json.dumps(content)}"})
         print(self.context)
-       # openai.api_key = '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'
-        openai.api_key = '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'
+
+    def Hacer_pregunta(self, prompt, max_tokens=100, temperature=0.5, top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0, stop=["\n"]):
+        # self.context.append( {'role':'system', 'content':"""first table: { "tableName": "employees", "fields": [ { "nombre": "ID_usr", "tipo": "int" }, { "nombre": "name", "tipo": "string" }]}"""})
+        # self.context.append( {'role':'system', 'content':"""second table:{"tableName": "salary","fields":[{"nombre": "ID_usr","type": "int"},{"name": "year","type": "date"},{"name": "salary","type": "float"}]}"""})
+        # self.context.append( {'role':'system', 'content':"""third table:{"tablename": "studies","fields": [{"name": "ID","type": "int"},{"name": "ID_usr","type": "int"},{"name": "educational level","type": "int"},{"name": "Institution","type": "string"},{"name": "Years","type": "date"}{"name": "Speciality","type": "string"}]}"""})
+        # self.context.append(self.Get_context_data())
+        self.context.append({'role': 'user', 'content': f"{prompt}."})
+        # self.context.append({'role': 'system', 'content': r"Remember your instructions as SQL Assistant."})
+        # print(self.context)
+        openai.api_key = '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'
+        # openai.api_key = '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'
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=self.context,
             max_tokens=max_tokens,
             temperature=temperature,
-            #top_p=top_p,
-            #frequency_penalty=frequency_penalty,
-            #presence_penalty=presence_penalty,
-            #stop=stop,
-            
-            )
-        self.context.append({'role':'assistant', 'content':f"{response}"})
+            # top_p=top_p,
+            # frequency_penalty=frequency_penalty,
+            # presence_penalty=presence_penalty,
+            # stop=stop,
+
+        )
+        self.context.append({'role': 'assistant', 'content': f"{response}"})
         message = response.choices[0].message.content
         index_inicio = message.upper().index("SELECT")
         index_final = message.index(';')
--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 from flask import Flask,jsonify
 from flask_cors import CORS
-from Models.Busqueda import Busqueda
+# from Models.Busqueda import Busqueda
 from Models.AIQuery import AIQuery
 
 
 app = Flask(__name__)
 CORS(app)
-busqueda_instance = Busqueda()
+# busqueda_instance = Busqueda()
 ai_instalce = AIQuery()
+ai_instalce.add_contents()
 
-@app.route('/api/busqueda')
-def GetBusqueda():
-    open_file = busqueda_instance.OpenFile()
-    col_names = busqueda_instance.names_col_cliente
-    read_ids = busqueda_instance.ReadDataFromExcel(open_file=open_file,name_columns=col_names)
-    result_query = busqueda_instance.HacerBusqueda(list_ids=read_ids)
-    busqueda_instance.GuardarDatos(df_result=result_query)
-    #return result_query.to_json(orient='records')
-    return result_query.to_json(orient='records')
+# @app.route('/api/busqueda')
+# def GetBusqueda():
+#     open_file = busqueda_instance.OpenFile()
+#     col_names = busqueda_instance.names_col_cliente
+#     read_ids = busqueda_instance.ReadDataFromExcel(open_file=open_file,name_columns=col_names)
+#     result_query = busqueda_instance.HacerBusqueda(list_ids=read_ids)
+#     busqueda_instance.GuardarDatos(df_result=result_query)
+#     #return result_query.to_json(orient='records')
+#     return result_query.to_json(orient='records')
 
 
 @app.route('/api/ai')
 def GetQuestion():
-    response = ai_instalce.Hacer_pregunta(prompt="Give me the name of the 5 employees with higher salary")
+    response = ai_instalce.Hacer_pregunta(prompt="Give me the id of the 5 articulos the type 01")
+    # response = ai_instalce.Get_context_data()
     return response
 
 if __name__ == "__main__":
